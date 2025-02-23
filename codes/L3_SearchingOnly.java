bb193c614114fcc0a4bf8b91e0631ff3b888f497
dinesh13p <159114512+dinesh13p@users.noreply.github.com>
2025-02-23 14:16:52 +0545

    Push

--- codes/L3_SearchingOnly.java
+++ codes/L3_SearchingOnly.java
@@ -1,36 +1,21 @@
 import java.util.Scanner;
 
-public class L3_Searching {
+public class L3_SearchingOnly {
 
-    public void display(int[] array)
-    {
-        System.out.println("Indexes are : ");
-        for(int element:array)
-        {
-            System.out.println(element);
-        }
-    }
-
-    public int[] Search(int[] arr, int key) {
+    public void Search(int[] arr, int key) {
         int n = arr.length;
         boolean found = false;
-        int[] array = new int[arr.length];
-        int index = 0;
+
         for (int i = 0; i < n; i++) {
             if (arr[i] == key) {
                 found = true;
-                array[index] = i;
-                index++;
                 System.out.println("Element found at index: " + i);
             }
         }
 
-        
         if (!found) {
             System.out.println("Element not found in array");
         }
-
-        return array;
     }
 
     public static void main(String[] args) {
@@ -39,7 +24,7 @@ public class L3_Searching {
         System.out.print("Enter number of elements: ");
         int n = scanner.nextInt();
         int[] arr = new int[n];
-        
+
         System.out.println("Enter the elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
@@ -49,9 +34,7 @@ public class L3_Searching {
         int key = scanner.nextInt();
 
         L3_Searching searchObj = new L3_Searching();
-       int[] array =  searchObj.Search(arr, key);
-
-       searchObj.display(array);
+        searchObj.Search(arr, key);
 
         scanner.close();
     }
